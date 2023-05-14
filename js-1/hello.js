f4bbc983731e49e00cdd4fe4059e1259977d3cfc
carlos <karlozhyun12@hotmail.com>
2023-05-14 10:59:47 -0500

    solucion

---
+++ js-1/hello.js
@@ -0,0 +1,6 @@
+function hola (nombre) {
+    return "hola" + nombre + "!"
+}
+console.log(hola("Pedro"))
+console.log(hola("Juan"))
+console.log(hola(""))
---
+++ js-1/sum.js
@@ -0,0 +1,6 @@
+function sum(numero){
+return (numero * (numero + 1)) / 2
+}
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
